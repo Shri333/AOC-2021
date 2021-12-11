c68cbd3eb09148033663e1b008442187e5eb2916
Shri333 <dshrihan@gmail.com>
2021-12-11 14:40:12 -0500

    refactor day11/puzzle2

--- day11/puzzle2.go
+++ day11/puzzle2.go
@@ -1,11 +1,16 @@
 package day11
 
 func puzzle2(grid [][]int) int {
-	for step := 1; ; step++ {
+	step := 1
+	for {
 		increaseEnergyLevel(grid)
 		flashes := flashOctupuses(grid)
 		if flashes == len(grid)*len(grid[0]) {
-			return step
+			break
 		}
+
+		step++
 	}
+
+	return step
 }
