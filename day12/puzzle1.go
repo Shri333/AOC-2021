aaf91d3e33485ba8036bedc115756f4deeeca895
Shri333 <dshrihan@gmail.com>
2021-12-12 18:52:31 -0500

    clean up sol 1 day 12

--- day12/puzzle1.go
+++ day12/puzzle1.go
@@ -1,58 +1,31 @@
 package day12
 
-import (
-	"unicode"
-)
-
-type set struct {
-	data map[string]bool
-}
-
-func (s *set) contains(element string) bool {
-	_, ok := s.data[element]
-	return ok
-}
-
-func (s *set) add(element string) bool {
-	if s.contains(element) {
-		return false
-	}
-
-	s.data[element] = true
-	return true
-}
-
-func (s *set) remove(element string) bool {
-	if !s.contains(element) {
-		return false
-	}
-
-	delete(s.data, element)
-	return true
-}
+import "unicode"
 
 func puzzle1(graph map[string][]string) int {
-	return dfs1(graph, "start", &set{make(map[string]bool)})
+	return dfs1(graph, "start", make(map[string]bool))
 }
 
-func dfs1(graph map[string][]string, current string, visited *set) int {
+func dfs1(graph map[string][]string, current string, visited map[string]bool) int {
 	if current == "end" {
 		return 1
 	}
 
-	if isLowerCase(current) {
-		visited.add(current)
-	}
-
 	paths := 0
 	for _, node := range graph[current] {
-		if !visited.contains(node) {
+		if node == "start" {
+			continue
+		}
+
+		if isLowerCase(node) {
+			if _, ok := visited[node]; isLowerCase(node) && !ok {
+				visited[node] = true
+				paths += dfs1(graph, node, visited)
+				delete(visited, node)
+			}
+		} else {
 			paths += dfs1(graph, node, visited)
 		}
-	}
-
-	if isLowerCase(current) {
-		visited.remove(current)
 	}
 
 	return paths
